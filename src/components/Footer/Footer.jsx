25876db047c2794542027d161a47bbdb6d1705ce
shakiba vakili <shakibvakili@gmail.com>
2023-10-30 16:09:35 +0330

    make footer responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,15 +9,15 @@ function Footer() {
       <div className="f-wrapper">
         <div className="innerWidth paddings flexCenter f-container">
           <div className="flexColStart f-left">
-            <img src="./logo2.png" alt="" width={90} />
-            <span className="secondartText">
+            <img src="./logo2.png" alt="" className="footerImg"/>
+            <span className="secondaryText">
               Lorem ipsum dolor sit amet.
               <br />
               Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               <br />
             </span>
           </div>
-          <div className="flexColStart f-right">
+          <div className="flexColEnd f-right">
             <div className="flexColEnd f-menu">
               <Link to="/about" className="navbar-link">
                 About Us
