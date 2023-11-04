964d5fe5329bbf23b2fb14da0fcb88964323b040
shakiba vakili <shakibvakili@gmail.com>
2023-11-04 11:56:28 +0330

    update privacy policy and terms of service

--- src/components/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,15 +1,66 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-
+import { motion } from "framer-motion";
+import './PrivacyPolicy.css'
 function PrivacyPolicy() {
   return (
     <div>
       <Navbar />
       <div className="pp-wrapper">
-        <div className="pp-container paddings innerWidth flexCenter">
-          <h2>PrivacyPolicy</h2>
-        </div>
+        <motion.div
+          initial={{ x: "5rem", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 2, type: "spring" }}
+          className="pp-container paddings innerWidth flexColCenter"
+        >
+          <h5 className="primaryTextOrange h5Headers flexEnd">Privacy Policy</h5>
+          <div className="flexColEnd pp-text secondaryTextGray">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Dignissimos aliquid rem fuga placeat sequi, fugiat dolor illo!
+              Dolores odio perspiciatis, ea et vero doloribus, quae aperiam
+              similique vitae iusto excepturi exercitationem commodi recusandae
+              quaerat assumenda repellat fugit, labore nihil praesentium
+              asperiores natus atque a voluptate deleniti? Minus ea fuga
+              debitis.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Dignissimos aliquid rem fuga placeat sequi, fugiat dolor illo!
+              Dolores odio perspiciatis, ea et vero doloribus, quae aperiam
+              similique vitae iusto excepturi exercitationem commodi recusandae
+              quaerat assumenda repellat fugit, labore nihil praesentium
+              asperiores natus atque a voluptate deleniti? Minus ea fuga
+              debitis.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Dignissimos aliquid rem fuga placeat sequi, fugiat dolor illo!
+              Dolores odio perspiciatis, ea et vero doloribus, quae aperiam
+              similique vitae iusto excepturi exercitationem commodi recusandae
+              quaerat assumenda repellat fugit, labore nihil praesentium
+              asperiores natus atque a voluptate deleniti? Minus ea fuga
+              debitis.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Dignissimos aliquid rem fuga placeat sequi, fugiat dolor illo!
+              Dolores odio perspiciatis, ea et vero doloribus, quae aperiam
+              similique vitae iusto excepturi exercitationem commodi recusandae
+              quaerat assumenda repellat fugit, labore nihil praesentium
+              asperiores natus atque a voluptate deleniti? Minus ea fuga
+              debitis.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Dignissimos aliquid rem fuga placeat sequi, fugiat dolor illo!
+              Dolores odio perspiciatis, ea et vero doloribus, quae aperiam
+              similique vitae iusto excepturi exercitationem commodi recusandae
+              quaerat assumenda repellat fugit, labore nihil praesentium
+              asperiores natus atque a voluptate deleniti? Minus ea fuga
+              debitis.
+            </p>
+          </div>
+        </motion.div>
       </div>
       {/* Your Home page content */}
       <Footer />
