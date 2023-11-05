1c4d1618c0d450eeea185a16b1db64597740a5bf
shakiba vakili <shakibvakili@gmail.com>
2023-11-05 13:57:35 +0330

    update ...3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'; // Import the useLocation hook
+import './App.css';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Product from './components/Product/Product';
-import PrivacyPolicy from './components/PrivacyPolicy/PrivacyPolicy'
-import TermsOfService from './components/TermsOfService/TermsOfService'
-import Careers from './components/Careers/Careers'
-import BusinessPractices from './components/BusinessPractices/BusinessPractices'
+import PrivacyPolicy from './components/PrivacyPolicy/PrivacyPolicy';
+import TermsOfService from './components/TermsOfService/TermsOfService';
+import Careers from './components/Careers/Careers';
+import BusinessPractices from './components/BusinessPractices/BusinessPractices';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to the top when the route changes
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop /> {/* Include the ScrollToTop component */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
